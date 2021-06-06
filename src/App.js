3f45985ce0e197b9daba7ef6054a52952234128e
b0829065 <b0829065@cgu.edu.tw>
2021-06-06 17:57:26 +0800

    pan web design

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import home from './components/pages/home';
+import producer from './components/pages/producer';
+import stock from './components/pages/stock';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={home} />
+          <Route path='/producer' component={producer} />
+          <Route path='/stock' component={stock} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/producer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function producer() {
+  return <h1 className='producer'>producer</h1>;
+}
